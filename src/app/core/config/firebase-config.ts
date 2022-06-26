cf3139e4891128565aeaadf3809c915bc8c3420b
vaisak <vaisakp07@gmail.com>
2022-06-26 12:00:58 +0530

    installed firebase

---
+++ src/app/core/config/firebase-config.ts
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAXjwWsD2rIVihxJreVO4rCo7PnSc1idXk",
+  authDomain: "clone-angular-341d5.firebaseapp.com",
+  projectId: "clone-angular-341d5",
+  storageBucket: "clone-angular-341d5.appspot.com",
+  messagingSenderId: "28131280198",
+  appId: "1:28131280198:web:044abe22eb9b8c9bde5147"
+};
+const app = initializeApp(firebaseConfig);
